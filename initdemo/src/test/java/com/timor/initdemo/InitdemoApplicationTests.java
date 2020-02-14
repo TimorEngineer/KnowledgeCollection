6ad4ab964f145f0888546c7c4ea4a53bb64961d0
xudong <xudong@wismake.com>
2020-02-14 15:42:32 +0800

    springBoot初探

---
+++ initdemo/src/test/java/com/timor/initdemo/InitdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timor.initdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
